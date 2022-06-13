120c6712423a60a9118f206ed34b773e84b117f7
Zacharia <ndunguzachary24@gmail.com>
2022-06-13 15:35:46 +0300

    commit tree fe62dfadd06b4d3162728e045766732d6364f896

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+// const sting =[]
+
+// function writeCards (sting){
+//     for (let i = 0; i <  sting.length; i++ ) {
+//         sting.push(`Thank you, ${sting[i]}, for the wonderful birthday gift`)
+
+//     }
+// }
+
+
+
+function countDown(){
+  let  count= 10
+    while (count >= 0){
+        console.log(count --)
+    }
+   
+}
